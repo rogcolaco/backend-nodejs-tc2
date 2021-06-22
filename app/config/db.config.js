c67add9431e795ae04568f593fa2575236d6e1ac
Rogerio Colaco <colaco.rogerio@gmail.com>
2021-06-22 17:22:15 -0300

    testes de conexao

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     url: "mongodb+srv://rogerio:process.env.atlasPass@cluster0.jb2bi.mongodb.net/dbtc2?retryWrites=true&w=majority"
+    /* const MongoClient = require('mongodb').MongoClient,
+    const uri = "mongodb+srv://rogerio:aqua578Man@cluster0.jb2bi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }),
+    client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    }), */
 }
